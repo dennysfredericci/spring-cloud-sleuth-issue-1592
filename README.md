bd2d86be3718674054ebc8e82f3c272bd5d4ea46
Dennys Fredericci <dennys.fredericci@kbc.be>
2020-07-14 22:52:06 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spring Cloud Sleuth - Issue #1592
+
+This project was created to reproduce the issue [#1592](https://github.com/spring-cloud/spring-cloud-sleuth/issues/1592)
+
+### Running this Project
+
+To reproduce the issue just add the environment variable below and run the main method.
+
+LOGGING_LEVEL=INFO
+
+
---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RestController
+    class HomeController {
+
+        @RequestMapping("/home")
+        public String home() {
+            log.warn("aeeee");
+            return "Hello from Spring";
+        }
+
+    }
+
+}
